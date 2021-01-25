da2c752342048809d7aea3ec6d1e4c9e841a33eb
chamano sidney <chamano.sr@gmail.com>
2021-01-25 12:14:30 +0200

    register a user

--- UserReg/UserReg/settings.py
+++ UserReg/UserReg/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/account/dashboard/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
