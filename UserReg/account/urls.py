ac5ed21724316a50bd329c038972f8356589b61e
chamano sidney <chamano.sr@gmail.com>
2021-01-25 11:03:19 +0200

    creating views

--- UserReg/account/urls.py
+++ UserReg/account/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.indexView, name="home"),
     path('dashboard/',views.dashboardView, name="dashboard"),
-    path('login/',),
-    path('register',),
-    path('logout/',),
+    # path('login/',views.loginView, name="login"),
+    path('register',views.registerView, name="register"),
+    # path('logout/',views.logoutView, name="logout"),
 ]
